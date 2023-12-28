f9b3442965868753b2b83990644a498760de9d04
rayhannaufal <rayhan.naufal55@gmail.com>
2023-12-28 11:52:37 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <el-menu
+        :default-active="activeIndex"
+        class="el-menu-demo"
+        mode="horizontal"
+        @select="handleSelect"
+    >
+        <el-menu-item index="0">
+            <img 
+                style="width: 230px"
+                src="./assets/logo.png"
+                alt="logo"
+            />
+        </el-menu-item>
+        <el-menu-item style="width: 60vw">
+        </el-menu-item>
+        <el-menu-item index="1" to="/">Beranda</el-menu-item>
+        <el-menu-item index="2">Kelas Prakerja</el-menu-item>
+        <el-menu-item index="3">Privacy</el-menu-item>
+        <el-menu-item index="4">Kelas Saya</el-menu-item>
+    </el-menu>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="/beranda">Beranda</router-link>
+  </nav>
+  <router-view/>
+</template>
+
+<script lang="ts" setup>
+  import { ref } from 'vue'
+
+  const activeIndex = ref('1')
+  const handleSelect = (key: string, keyPath: string[]) => {
+    console.log(key, keyPath)
+  }
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/views/BerandaView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="beranda">
+        <h2>INI adalah Halaman Beranda</h2>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'BerandaView'
+}
+</script>
+
+<style>
+
+</style>
